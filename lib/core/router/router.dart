491f773081f8608f9c4f1adfb0b7dc72c7657903
kimhyungmin <jeen14rla@gmail.com>
2024-08-25 16:30:55 +0900

    feat: router 추가

---
+++ lib/core/router/router.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_image_cropper/presentation/album/album_screen.dart';
+import 'package:flutter_image_cropper/presentation/camera/camera_screen.dart';
+import 'package:flutter_image_cropper/presentation/edited/edited_screen.dart';
+import 'package:flutter_image_cropper/presentation/main/main_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const MainScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'album',
+          builder: (context, state) {
+            return const AlbumScreen();
+          },
+        ),
+        GoRoute(
+          path: 'camera',
+          builder: (context, state) {
+            return const CameraScreen();
+          },
+        ),
+        GoRoute(
+          path: 'edited',
+          builder: (context, state) {
+            return const EditedScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
