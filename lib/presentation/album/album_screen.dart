081459b214cb981d5f4e1ffa305b5511259ee9bd
kimhyungmin <jeen14rla@gmail.com>
2024-08-25 21:20:42 +0900

    feat: 편집 버튼 추가

--- lib/presentation/album/album_screen.dart
+++ lib/presentation/album/album_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_image_cropper/core/router/router.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AlbumScreen extends StatefulWidget {
   const AlbumScreen({super.key});
@@ -8,11 +14,44 @@ class AlbumScreen extends StatefulWidget {
 }
 
 class _AlbumScreenState extends State<AlbumScreen> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Album'),
+      ),
+      body: _image == null
+          ? const Center(
+              child: Text('No Images'),
+            )
+          : Column(
+              children: [
+                Image.file(_image!),
+                TextButton(
+                  onPressed: () {
+                    context.go('/editing');
+                  },
+                  child: const Text('편집'),
+                ),
+              ],
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImage,
+        tooltip: '이미지 선택',
+        child: const Icon(Icons.add_a_photo),
       ),
     );
   }
