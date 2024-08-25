7cfd5a26eec7d4caab0488f877b727660ce79f56
kimhyungmin <jeen14rla@gmail.com>
2024-08-25 15:39:37 +0900

    feat: 앨범, 카메라 아이콘 텍스트 추가

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -5,6 +5,33 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 300,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(Icons.image),
+                    Text('앨범'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(Icons.camera_alt),
+                    Text('카메라'),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
