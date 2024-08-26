442a74967bf9fa95897649caa158dfb98a25e22a
kimhyungmin <jeen14rla@gmail.com>
2024-08-26 15:08:21 +0900

    feat: cropImage 추가

--- lib/presentation/album/album_screen.dart
+++ lib/presentation/album/album_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AlbumScreen extends StatefulWidget {
@@ -14,6 +15,9 @@ class AlbumScreen extends StatefulWidget {
 class _AlbumScreenState extends State<AlbumScreen> {
   File? _image;
 
+  // XFile? _pickedFile; // 선택된 이미지 파일
+  CroppedFile? _croppedFile; // 크롭된 이미지 파일
+
   Future<void> _pickImage() async {
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -22,6 +26,51 @@ class _AlbumScreenState extends State<AlbumScreen> {
       setState(() {
         _image = File(pickedFile.path);
       });
+    }
+  }
+
+  Future<void> _cropImage() async {
+    if (_image != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: _image!.path,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: false,
+            aspectRatioPresets: [
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio4x3,
+            ],
+          ),
+          IOSUiSettings(
+            title: 'Cropper',
+            aspectRatioPresets: [
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio4x3,
+            ],
+          ),
+          WebUiSettings(
+            context: context,
+            presentStyle: WebPresentStyle.dialog,
+            size: const CropperSize(
+              width: 520,
+              height: 520,
+            ),
+          ),
+        ],
+      );
+      if (croppedFile != null) {
+        setState(() {
+          _croppedFile = croppedFile;
+        });
+      }
     }
   }
 
@@ -40,7 +89,8 @@ class _AlbumScreenState extends State<AlbumScreen> {
                 Image.file(_image!),
                 TextButton(
                     onPressed: () {
-                      context.go('/album/editing', extra: _image);
+                      // context.go('/album/editing');
+                      _cropImage();
                     },
                     child: const Text('편집'))
               ],
