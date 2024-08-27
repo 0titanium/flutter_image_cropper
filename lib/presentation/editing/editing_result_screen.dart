c373aaaf278373daba51d1fbc8b2f76509a503a8
kimhyungmin <jeen14rla@gmail.com>
2024-08-27 11:19:39 +0900

    feat: storage 업로드 기능 추가

--- lib/presentation/editing/editing_result_screen.dart
+++ lib/presentation/editing/editing_result_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 
@@ -25,6 +26,20 @@ class _EditingResultScreenState extends State<EditingResultScreen> {
     croppedFile = widget.image;
   }
 
+  Future saveImage() async {
+    if (croppedFile == null) return;
+
+    final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final Reference storageReference =
+        FirebaseStorage.instance.ref().child('images/$fileName.jpg');
+
+    try {
+      await storageReference.putFile(File(croppedFile!.path));
+    } catch (e) {
+      print('Error uploading image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +50,7 @@ class _EditingResultScreenState extends State<EditingResultScreen> {
         child: Image.file(File(croppedFile!.path)),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: saveImage,
         tooltip: '저장',
         child: const Icon(Icons.save),
       ),
