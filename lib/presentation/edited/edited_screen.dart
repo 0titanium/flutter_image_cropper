b9d9bccfc1c56c221687e8723cdd7d3ea5590bf8
kimhyungmin <jeen14rla@gmail.com>
2024-08-27 15:15:45 +0900

    feat: 디테일 페이지 라우팅 추가

--- lib/presentation/edited/edited_screen.dart
+++ lib/presentation/edited/edited_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class EditedScreen extends StatefulWidget {
   const EditedScreen({super.key});
@@ -8,11 +10,53 @@ class EditedScreen extends StatefulWidget {
 }
 
 class _EditedScreenState extends State<EditedScreen> {
+  List<String> _imageUrls = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  Future<void> _loadImages() async {
+    final ListResult result =
+        await FirebaseStorage.instance.ref('images').listAll();
+
+    final List<String> urlList = await Future.wait(
+      result.items.map((ref) => ref.getDownloadURL()),
+    );
+
+    setState(() {
+      _imageUrls = urlList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edited'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+        ),
+        itemCount: _imageUrls.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              context.go('/edited/resultDetail', extra: _imageUrls[index]);
+              debugPrint(_imageUrls[index]);
+            },
+            // => _showFullImage(context, _imageUrls[index]),
+            child: Image.network(
+              _imageUrls[index],
+              fit: BoxFit.cover,
+            ),
+          );
+        },
       ),
     );
   }
