6921c3a9ecdda9152b249d13b1c6a079a62d3680
kimhyungmin <jeen14rla@gmail.com>
2024-08-30 23:00:59 +0900

    feat: 라우터 정리

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,17 +1,8 @@
 import 'package:flutter_image_cropper/data/image_data_source.dart';
-import 'package:flutter_image_cropper/presentation/album/album_screen.dart';
-import 'package:flutter_image_cropper/presentation/album/album_view_model.dart';
-import 'package:flutter_image_cropper/presentation/camera/camera_screen.dart';
-import 'package:flutter_image_cropper/presentation/camera/camera_view_model.dart';
-import 'package:flutter_image_cropper/presentation/edited/edited_screen.dart';
-import 'package:flutter_image_cropper/presentation/edited/edited_view_model.dart';
-import 'package:flutter_image_cropper/presentation/editing/editing_result_screen.dart';
-import 'package:flutter_image_cropper/presentation/editing/editing_result_view_model.dart';
 import 'package:flutter_image_cropper/presentation/main/main_screen.dart';
 import 'package:flutter_image_cropper/presentation/main/main_view_model.dart';
 import 'package:flutter_image_cropper/presentation/result_detail/result_detail_screen.dart';
 import 'package:go_router/go_router.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:provider/provider.dart';
 
 final GoRouter router = GoRouter(
@@ -32,69 +23,6 @@ final GoRouter router = GoRouter(
             return ResultDetailScreen(imageUrl: state.extra as String);
           },
         ),
-        // GoRoute(
-        //   path: 'album',
-        //   builder: (context, state) {
-        //     return ChangeNotifierProvider(
-        //       create: (_) => AlbumViewModel(),
-        //       child: const AlbumScreen(),
-        //     );
-        //   },
-        //   routes: <RouteBase>[
-        //     GoRoute(
-        //       path: 'editingResult',
-        //       builder: (context, state) {
-        //         return ChangeNotifierProvider(
-        //           create: (_) => EditingResultViewModel(
-        //             croppedFile: state.extra as CroppedFile,
-        //           ),
-        //           child: const EditingResultScreen(),
-        //         );
-        //       },
-        //     ),
-        //   ],
-        // ),
-        // GoRoute(
-        //   path: 'camera',
-        //   builder: (context, state) {
-        //     return ChangeNotifierProvider(
-        //       create: (_) => CameraViewModel(),
-        //       child: const CameraScreen(),
-        //     );
-        //   },
-        //   routes: <RouteBase>[
-        //     GoRoute(
-        //       path: 'editingResult',
-        //       builder: (context, state) {
-        //         return ChangeNotifierProvider(
-        //           create: (_) => EditingResultViewModel(
-        //             croppedFile: state.extra as CroppedFile,
-        //           ),
-        //           child: const EditingResultScreen(),
-        //         );
-        //       },
-        //     ),
-        //   ],
-        // ),
-        // GoRoute(
-        //   path: 'edited',
-        //   builder: (context, state) {
-        //     return ChangeNotifierProvider(
-        //       create: (_) => EditedViewModel(
-        //         imageDataSource: ImageDataSource(),
-        //       )..loadImages(),
-        //       child: const EditedScreen(),
-        //     );
-        //   },
-        //   routes: <RouteBase>[
-        //     GoRoute(
-        //       path: 'resultDetail',
-        //       builder: (context, state) {
-        //         return ResultDetailScreen(imageUrl: state.extra as String);
-        //       },
-        //     )
-        //   ],
-        // ),
       ],
     ),
   ],
