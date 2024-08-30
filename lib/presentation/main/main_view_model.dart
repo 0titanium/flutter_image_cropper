9a6978c9f8b0701550f39e422bc0bca4ae200e2e
kimhyungmin <jeen14rla@gmail.com>
2024-08-30 23:00:21 +0900

    feat: 이미지 선택 메서드 통합, 이미지 저장 추가

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_cropper/data/image_data_source.dart';
 import 'package:image_cropper/image_cropper.dart';
@@ -24,11 +23,6 @@ class MainViewModel extends ChangeNotifier {
 
   // vars pick image & crop image
   File? _image;
-  CroppedFile? _croppedFile;
-
-  File? get image => _image;
-
-  CroppedFile? get croppedFile => _croppedFile;
 
   // about firebase storage
   void loadImages() async {
@@ -49,24 +43,14 @@ class MainViewModel extends ChangeNotifier {
   }
 
   // about pick image & crop image
-  Future<void> pickImage() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+  Future<void> pickImage(String types) async {
+    final pickedFile = types == 'album'
+        ? await ImagePicker().pickImage(source: ImageSource.gallery)
+        : await ImagePicker().pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
       _image = File(pickedFile.path);
-      cropImage();
-      notifyListeners();
-    }
-  }
-
-  Future<void> pickImageFromPhoto() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-
-    if (pickedFile != null) {
-      _image = File(pickedFile.path);
-      cropImage();
+      await cropImage();
       notifyListeners();
     }
   }
@@ -101,9 +85,8 @@ class MainViewModel extends ChangeNotifier {
         ],
       );
       if (croppedFile != null) {
-        // context.go('/album/editingResult', extra: croppedFile);
-        _croppedFile = croppedFile;
-        notifyListeners();
+        await _imageDataSource.saveImage(croppedFile);
+        loadImages();
       }
     }
   }
